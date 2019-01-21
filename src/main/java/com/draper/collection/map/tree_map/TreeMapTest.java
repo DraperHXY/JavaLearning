daf66935672edea89dcf378ac67d9a54f9f761fb
Draper_HXY <draper_hxy@163.com>
2019-01-21 20:49:13 +0800

    TreeMap

--- src/main/java/com/draper/collection/map/tree_map/TreeMapTest.java
+++ src/main/java/com/draper/collection/map/tree_map/TreeMapTest.java
@@ -1,33 +1,88 @@
 package com.draper.collection.map.tree_map;
 
+
+import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 public class TreeMapTest {
-    public static void main(String[] agrs){
-        //自然顺序比较
-        naturalSort();
-    }
-    //自然排序顺序：
-    public static void naturalSort(){
-        //第一种情况：Integer对象
-        TreeMap<Integer,String> treeMapFirst = new TreeMap<Integer, String>();
-        treeMapFirst.put(1,"jiaboyan");
-        treeMapFirst.put(6,"jiaboyan");
-        treeMapFirst.put(3,"jiaboyan");
-        treeMapFirst.put(10,"jiaboyan");
-        treeMapFirst.put(7,"jiaboyan");
-        treeMapFirst.put(13,"jiaboyan");
-        System.out.println(treeMapFirst.toString());
 
-        //第二种情况:SortedTest对象
-        TreeMap<SortedTest,String> treeMapSecond = new TreeMap<SortedTest, String>();
-        treeMapSecond.put(new SortedTest(10),"jiaboyan");
-        treeMapSecond.put(new SortedTest(1),"jiaboyan");
-        treeMapSecond.put(new SortedTest(13),"jiaboyan");
-        treeMapSecond.put(new SortedTest(4),"jiaboyan");
-        treeMapSecond.put(new SortedTest(0),"jiaboyan");
-        treeMapSecond.put(new SortedTest(9),"jiaboyan");
-        System.out.println(treeMapSecond.toString());
+
+    public static void main(String[] args) {
+        new TreeMapTest().test();
     }
+
+    private void test() {
+        TreeMap<Student, String> treeMap = new TreeMap<>();
+        Student student1 = new Student(12);
+        Student student2 = new Student(11);
+        Student student3 = new Student(45);
+        Student student4 = new Student(82);
+        Student student5 = new Student(66);
+
+        System.out.println(student1 + "\thashCode = " + student1.hashCode());
+        System.out.println(student2 + "\thashCode = " + student2.hashCode());
+        System.out.println(student3 + "\thashCode = " + student3.hashCode());
+        System.out.println(student4 + "\thashCode = " + student4.hashCode());
+        System.out.println(student5 + "\thashCode = " + student5.hashCode());
+
+        treeMap.put(student1, "小米12");
+        treeMap.put(student2, "小米11");
+        treeMap.put(student3, "小米45");
+        treeMap.put(student4, "小米82");
+        treeMap.put(student5, "小米66");
+
+        System.out.println(treeMap.size());
+
+        Set<Map.Entry<Student, String>> entries = treeMap.entrySet();
+        for (Map.Entry<Student, String> entry : entries) {
+            System.out.println(entry.getKey());
+            System.out.println(entry.getValue());
+
+        }
+
+    }
+
+    private class Student implements Comparable<Student> {
+
+        private int age;
+
+        public int getAge() {
+            return age;
+        }
+
+        public Student(int age) {
+            this.age = age;
+        }
+
+        // TreeMap 的 key 的唯一性是通过 compareTo 这个接口来实现的
+        @Override
+        public int compareTo(Student o) {
+//            int num = this.age - o.getAge();
+//            //为0时候，两者相同：
+//            if (num == 0) {
+//                return 0;
+//                //大于0时，传入的参数小：
+//            } else if (num > 0) {
+//                return 1;
+//                //小于0时，传入的参数大：
+//            } else {
+//                return -1;
+//            }
+
+
+            //********************** 上面的是正确的，下面的是错误的
+
+            if (this == o) {
+                return 0;
+            } else {
+                if (this.age < o.getAge()) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+        }
+    }
+
 }
-
