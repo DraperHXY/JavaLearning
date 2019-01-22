dac657463df44d18a3ca9131243a6a4a5d3f2143
Draper <draper_hxy@gmail.com>
2019-01-22 21:24:40 +0800

    Test

--- src/main/java/com/draper/design_pattern/single/three/Singleton.java
+++ src/main/java/com/draper/design_pattern/single/three/Singleton.java
@@ -9,17 +9,56 @@ public class Singleton {
     }
 
     public static Singleton getInstance() {
-        if (singleton == null) {                        // single check
+        if (singleton == null) {// single check
+
+            System.out.println(".");
+
             synchronized (Singleton.class) {
-                if (singleton == null) {                // double check
+//                if (singleton == null) {                // double check
+                    try {
+                        Thread.sleep(5000);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
                     singleton = new Singleton();
-                }
+//                }
             }
+
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
         }
         return singleton;
     }
 
     public static void main(String[] args) {
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Singleton.getInstance());
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Singleton.getInstance());
+            }
+        }).start();
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                System.out.println(Singleton.getInstance());
+            }
+        }).start();
+
+
+
 
     }
 
