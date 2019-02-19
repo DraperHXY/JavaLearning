42f6cef3a540af9839d76335163d08c9e565e604
Draper_HXY <draper_hxy@163.com>
2019-02-19 11:00:46 +0800

    feat 输入输出流装饰者模式

--- src/main/java/com/draper/io/InputStreamToReader.java
+++ src/main/java/com/draper/io/InputStreamToReader.java
@@ -1,10 +1,12 @@
 package com.draper.io;
 
+
 import java.io.*;
+import java.util.zip.ZipInputStream;
 
 public class InputStreamToReader {
 
-    public static void main(String[] args) throws IOException {
+    public static void main1(String[] args) throws IOException {
         InputStream inputStream = new FileInputStream("fileA.txt");
         Reader reader = new InputStreamReader(inputStream);
         int data;
@@ -17,6 +19,47 @@ public class InputStreamToReader {
         while ((data2 = reader2.read()) != -1) {
             System.out.print((char) data2);
         }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedInputStream in = new BufferedInputStream((
+                new BufferedInputStream(
+                        new BufferedInputStream(
+                                new BufferedInputStream(
+                                        new BufferedInputStream(
+                                                new BufferedInputStream(
+                                                        new BufferedInputStream(
+                                                                new BufferedInputStream(
+                                                                        new BufferedInputStream(
+                                                                                new BufferedInputStream(
+                                                                                        new BufferedInputStream(
+                                                                                                new BufferedInputStream(
+                                                                                                        new BufferedInputStream(
+                                                                                                                new BufferedInputStream(
+                                                                                                                        new BufferedInputStream(
+                                                                                                                                new BufferedInputStream(
+                                                                                                                                        new FileInputStream("fileA.txt"))
+                                                                                                                        )
+                                                                                                                )
+                                                                                                        )
+                                                                                                )
+                                                                                        )
+                                                                                )
+                                                                        )
+                                                                )
+                                                        )
+                                                )
+                                        ))))));
+
+        byte[] data = new byte[1024];
+        int end;
+
+        while ((end = in.read(data)) != -1) {
+            String str = new String(data, 0, end);
+            System.out.println(str);
+        }
+
+
     }
 
 }
