3c6635734f45f07ecf3dc905bf0a60bac097ae1c
Draper_HXY <draper_hxy@163.com>
2019-02-21 18:18:59 +0800

    feat NIO Create Mult Level Directory
    NIO 创建多级目录

--- src/main/java/com/draper/nio/PathsTest.java
+++ src/main/java/com/draper/nio/PathsTest.java
@@ -1,4 +1,18 @@
 package com.draper.nio;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class PathsTest {
+
+    public static void main(String[] args) throws IOException {
+        Path path = Paths.get("~/bb");
+        path = Paths.get("img1/img2/img3/img4/img5");
+        if (!Files.exists(path)) {
+            Files.createDirectories(path);
+        }
+    }
+
 }
