4c910539bf76fbf099f7f44cb45c015777217ac8
Draper_HXY <draper.hxy@gmail.com>
2019-04-17 18:36:00 +0800

    feat Thread SingleCondition

---
+++ Thread/Lock/Condition/src/main/java/ThreadA.java
@@ -0,0 +1,17 @@
+/**
+ * @author draper_hxy
+ */
+public class ThreadA extends Thread {
+
+    private MyService service;
+
+    public ThreadA(MyService service) {
+        this.service = service;
+    }
+
+    @Override
+    public void run() {
+        service.await();
+    }
+
+}
